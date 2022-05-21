f96b780bfc191b9f73e19141e3b3553f406b6cfa
Noah Palin <noah.m.palin@gmail.com>
2022-05-21 12:44:28 +0000

    a function that compares if 2 arrays are identical

---
+++ eqArrays.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  //the message the assertion will output if it finds an error
+  let errorMessage = `${actual} !== ${expected}`;
+  //the following if statement adds the red circles if actual and expected don't match
+  if (actual !== expected) {
+    process.stdout.write("🔴🔴🔴");
+  }
+  console.assert(actual === expected, errorMessage);
+
+  //if the assertion passes, this if statement prints a message telling the user it passed
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+//this function only works if both arrays have the same length
+const eqArrays = function(arr1, arr2) {
+  //for loop will loop through the entire array
+  for (let i = 0; i < arr1.length; i++) {
+    //checks if element i in both arrays do not equal
+    if (arr1[i] !== arr2[i]) {
+      //if the if statement above was true, we return false as the arrays are not equal
+      return false;
+    }
+  }
+  //if each element in both arrays match and no different values are found,
+  //then the arrays must be identical so we return true
+  return true;
+};
+
+//test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+assertEqual(eqArrays(["How", "are", "you", "today?"], ["How", "are", "you", "today?"]), true); // => should PASS
+assertEqual(eqArrays([true, true, false], [true, true, false]), true); // => should PASS
+
+
+
+
+
